138f4d256bb4e8ff0529b31b50d0a961b831ccb5
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-11-02 23:47:19 -0300

    Adicionando arquivos para pipeline

--- src/main.tf
+++ src/main.tf
@@ -1,111 +1,129 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.56.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.56.0"
+        }
     }
-  }
-  backend "s3" {
-  }
+    backend "s3" {
+      # Definir as variáveis de ambiente no Jenkins
+    }
 }
 
+# Definindo o Provider
 provider "aws" {
-  region = var.aws_region
+    region = var.aws_region
 }
 
-resource "aws_vpc" "labs_vpc" {
-  cidr_block = var.vpc_cidr_block
+# Provisionando a VPC 
+resource "aws_vpc" "rwa_vpc_tj" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      Name = var.vpc_nome
+    }
+}
+
+# Provisionando a Subnet
+resource "aws_subnet" "rwa_subnet_tj" {
+  vpc_id = aws_vpc.rwa_vpc_tj.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.subnet_availability_zone
+
   tags = {
-    Name = var.vpc_name
+    Name = var.subnet_nome
   }
 }
 
-resource "aws_subnet" "labs_sub" {
-  vpc_id            = aws_vpc.labs_vpc.id
-  cidr_block        = var.sub_net_cidr_block
-  availability_zone = var.sub_net_availability_zone
-
+# Provisionando o Internet Gateway
+resource "aws_internet_gateway" "rwa_gw_tj" {
   tags = {
-    Name = var.sub_net_name
+    Name = var.internet_gateway_nome
   }
 }
 
-resource "aws_internet_gateway" "labs_gw" {
-  tags = {
-    Name = var.internet_gateway_name
-  }
+# Garantindo que o Internet Gateway seja associado a VPC
+resource "aws_internet_gateway_attachment" "rwa_gw_vpc_tj" {
+  internet_gateway_id = aws_internet_gateway.rwa_gw_tj.id
+  vpc_id = aws_vpc.rwa_vpc_tj.id
 }
 
-resource "aws_internet_gateway_attachment" "ig_vpc" {
-  internet_gateway_id = aws_internet_gateway.labs_gw.id
-  vpc_id              = aws_vpc.labs_vpc.id
+# Provisionando a Route Table
+resource "aws_route_table" "rwa-rt-tj" {
+  vpc_id = aws_vpc.rwa_vpc_tj.id
 }
 
-resource "aws_route_table_association" "association" {
-  subnet_id      = aws_subnet.labs_sub.id
-  route_table_id = aws_route_table.labs_rt.id
+resource "aws_route_table_association" "rwa_rt_atachar" {
+  subnet_id = aws_subnet.rwa_subnet_tj.id
+  route_table_id = aws_route_table.rwa-rt-tj.id
 }
 
-resource "aws_route_table" "labs_rt" {
-  vpc_id = aws_vpc.labs_vpc.id
+# Provisionando a Route Table
+resource "aws_route_table" "rwa_rt_nome" {
+  vpc_id = aws_vpc.rwa_vpc_tj.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.labs_gw.id
+    gateway_id = aws_internet_gateway.rwa_gw_tj.id
   }
 
   tags = {
-    Name = var.route_table_name
+    Name = var.route_table_nome
   }
 }
 
-data "aws_key_pair" "deployer" {
-  key_name = var.key_pair_name
+# Garantindo que será utilizado a Key Pair Devops Rodrigo Afonso
+data "aws_key_pair" "rwa_chave_tj" {
+  key_name = var.key_pair_nome
 }
-
-resource "aws_instance" "lab_ec2" {
-  ami                         = var.instance_ami
-  instance_type               = var.instance_type
-  key_name                    = data.aws_key_pair.deployer.key_name
-  subnet_id                   = aws_subnet.labs_sub.id
+  
+# Provisionando a Instancia AWS
+resource "aws_instance" "rwa_ec2_tj" {
+  ami = var.ec2_imagem_instancia
+  instance_type = var.ec2_tipo_instancia
+  key_name = data.aws_key_pair.rwa_chave_tj.key_name
+  subnet_id = aws_subnet.rwa_subnet_tj.id
   associate_public_ip_address = true
-  vpc_security_group_ids              = [aws_security_group.allow_all.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
-    Name = "lab-ec2"
+    Name = "srv-pipeline-tj"
   }
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+# Provisionando o Security Group
+resource "aws_security_group" "rwa_sg_tj" {
+  name = "allow_all"
   description = "Permitir o trafego de tudo"
-  vpc_id      = aws_vpc.labs_vpc.id
+  vpc_id = aws_vpc.rwa_vpc_tj.id
 
   tags = {
     Name = "allow_all"
   }
 }
 
-resource "aws_security_group_rule" "allow_all_ingress" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.allow_all.id
+# Garantindo que o Inbound esteja liberado para todos
+resource "aws_security_group_rule" "rwa_sg_ingress_tj" {
+  type = "ingress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = aws_security_group.rwa_sg_tj.id
 }
 
-resource "aws_security_group_rule" "allow_all_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.allow_all.id
+# Garantindo que o Outbound esteja liberado para todos
+resource "aws_security_group_rule" "rwa_sg_egress_tj" {
+  type = "egress"
+  from_port = "0"
+  to_port = "0"
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = aws_security_group.rwa_sg_tj.id
 }
 
+# Informando o IP Público da Instância
 output "public_ip" {
-  value = aws_instance.lab_ec2.public_ip
+  value = aws_instance.rwa_ec2_tj.public_ip
 }
--- src/variables.tf
+++ src/variables.tf
@@ -1,59 +1,59 @@
 variable "aws_region" {
-  type    = string
+  type = string
   default = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
+  default = "192.168.0.0/16"
 }
 
-variable "vpc_name" {
-  type    = string
-  default = "labs-vpc"
+variable "vpc_nome" {
+  type = string
+  default = "rwa-vpc-tj"
 }
 
-variable "sub_net_name" {
-  type    = string
-  default = "labs-sub"
+variable "subnet_nome" {
+  type = string
+  default = "rwa-subnet-tj"
 }
 
-variable "sub_net_cidr_block" {
-  type    = string
-  default = "10.0.0.0/24"
+variable "subnet_cidr_block" {
+  type = string
+  default = "192.168.0.1/24"
 }
 
-variable "sub_net_availability_zone" {
-  type    = string
-  default = "us-east-1a"
+variable "subnet_availability_zone" {
+  type = string
+  default = "us-aest-1a"
 }
 
-variable "internet_gateway_name" {
-  type    = string
-  default = "labs-gw"
+variable "internet_gateway_nome" {
+  type = string
+  default = "rwa-gw-tj"
 }
 
-variable "route_table_name" {
-  type    = string
-  default = "labs-rt"
+variable "route_table_nome" {
+  type = string
+  default = "rwa-rt-tj"
 }
 
-variable "key_pair_name" {
-  type    = string
-  default = "labs-key"
+variable "key_pair_nome" {
+  type = string
+  default = "key_devops_rodrigoafonso"
 }
 
-variable "instance_name" {
-  type    = string
-  default = "lab-ec2"
+variable "ec2_instancia_nome" {
+  type = string
+  default = "srv-pipeline-tj"
 }
 
-variable "instance_ami" {
-  type    = string
-  default = "ami-053b0d53c279acc90"
+variable "ec2_imagem_instancia" {
+  type = string
+  default = "ami-0866a3c8686eaeeba"
 }
 
-variable "instance_type" {
-  type    = string
+variable "ec2_tipo_instancia" {
+  type = string
   default = "t2.micro"
 }
